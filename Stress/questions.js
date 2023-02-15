2debc05be80777c620b1e52f00982c62ce3e7771
Shiraz Sabah <68498257+shirazsabah@users.noreply.github.com>
2023-02-15 21:07:12 +0000

    First commit

---
+++ Stress/questions.js
@@ -0,0 +1,38 @@
+    quiz = {
+            "info": {
+                          "name":    "Quiz",
+                          "main":    "",
+                          "level1":  "",
+                          "level2":  "",
+                          "level3":  "",
+                          "level4":  "",
+                          "level5":  "" // no comma here
+                  },
+            "questions": [
+                    { // Question 1 - Multiple Choice, Single True Answer
+                            "q": "In an isolated fracture of the greater tuberosity of the humerus, the fracture fragment is most likely to be displaced:",
+                            "a": [
+                                    {"option": "Superiorly and anteriorly, due to action of supraspinatous, infraspinatous and teres minor.",      "correct": false},
+                                    {"option": "Superiorly and anteriorly, due to the action of supraspinatous and subscapularis",     "correct": false},
+                                    {"option": "Inferiorly and posteriorly, due to the action of infraspinatous and teres minor.",      "correct": false},
+                                    {"option": "Superiorly and posteriorly, due the action of supraspinatous, infraspinatous and teres minor.",     "correct": true},
+                                    {"option": "Superiorly and posteriorly, due to supraspinatous and subscapsularis.",     "correct": false} // no comma here
+                            ],
+                            "correct": "<p><span>Correct!</span> The fragment is displaced superiorly and posteriorly, due the action of supraspinatous, infraspinatous and teres minor.</p>",
+                            "incorrect": "<p><span>That's not right.</span> The fragment is displaced superiorly and posteriorly, due the action of supraspinatous, infraspinatous and teres minor.</p>" // no comma here
+                    },
+                                        { // Question 2 - Multiple Choice, Single True Answer
+                            "q": "Displacement of the fragments of a humeral shaft fracture will be dependent on the location of the fracture compared to muscle insertions. If the proximal fragment adducted and distal fragment displaced laterally, this suggests a fracture location:",
+                            "a": [
+                                    {"option": "Anatomical neck of humerus", "correct": false},
+                                    {"option": "Humeral diaphysis proximal to pectoralis major insertion.", "correct": false},
+                                    {"option": "Humeral diaphysis distal to pectoralis major insertion and proximal to deltoid insertion.", "correct": true},
+                                    {"option": "Humeral diaphysis but distal to deltoid insertion.", "correct": false},
+                                    {"option": "Supracondylar humeral fracture.", "correct": false} // no comma here
+                            ],
+                            "correct": "<p><span>Correct!</span> This pattern of displacement suggests a fracture of the humeral diaphysis distal to pectoralis major insertion and proximal to deltoid insertion.</p>",
+                            "incorrect": "<p><span>That's not right.</span> This pattern of displacement suggests a fracture of the humeral diaphysis distal to pectoralis major insertion and proximal to deltoid insertion.</p>" // no comma here
+                    },
+                    // more questions here
+            ]
+    }
